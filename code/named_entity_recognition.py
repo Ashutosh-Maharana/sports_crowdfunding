3755834f616e46b9c9294f7fc1f80edcc1d73c4f
Ashutosh-Maharana <ashutosh.maharana@okstate.edu>
2023-05-02 18:46:42 -0500

    modified:   code/named_entity_recognition.py 	modified:   data/text_analysis_data/ner_results.csv

--- code/named_entity_recognition.py
+++ code/named_entity_recognition.py
@@ -19,30 +19,41 @@ df = pd.read_csv("{}\\data\\clean_data\\final_dataset_textanalysis_sentiment_sco
 english_df = df[df['is_english'] == 1].copy()
 # %%
 # create columns to store named entity recognition results
-for entity in ['Person', 'Organization', 'Geo', 'Group', 'Event', 'Product']:
+entities = ['PERSON', 'ORG', 'PRODUCT', 'EVENT', 'MONEY', 'DATE', 'TIME', 'GPE', 'LOC', 'NORP']
+for entity in entities:
     english_df[f'{entity}_Entity'] = ''
 
 # loop through each story and perform named entity recognition using spaCy
 for i, story in english_df['Story_Original'].iteritems():
     doc = nlp(story)
     for ent in doc.ents:
-        if ent.label_ == 'PERSON':
-            english_df.at[i, 'Person_Entity'] += f"{ent.text} ({ent.label_}), "
-        elif ent.label_ == 'ORG':
-            english_df.at[i, 'Organization_Entity'] += f"{ent.text} ({ent.label_}), "
-        elif ent.label_ in ['GPE', 'LOC', 'FAC']:
-            english_df.at[i, 'Geo_Entity'] += f"{ent.text} ({ent.label_}), "
-        elif ent.label_ == 'NORP':
-            english_df.at[i, 'Group_Entity'] += f"{ent.text} ({ent.label_}), "
-        elif ent.label_ == 'EVENT':
-            english_df.at[i, 'Event_Entity'] += f"{ent.text} ({ent.label_}), "
-        elif ent.label_ == 'PRODUCT':
-            english_df.at[i, 'Product_Entity'] += f"{ent.text} ({ent.label_}), "
+        if ent.label_ in ['PERSON']:
+            english_df.at[i, 'PERSON_Entity'] += f"{ent.text} ({ent.label_}), "
+        elif ent.label_ in ['ORG']:
+            english_df.at[i, 'ORG_Entity'] += f"{ent.text} ({ent.label_}), "
+        elif ent.label_ in ['PRODUCT']:
+            english_df.at[i, 'PRODUCT_Entity'] += f"{ent.text} ({ent.label_}), "
+        elif ent.label_ in ['EVENT']:
+            english_df.at[i, 'EVENT_Entity'] += f"{ent.text} ({ent.label_}), "
+        elif ent.label_ in ['MONEY']:
+            english_df.at[i, 'MONEY_Entity'] += f"{ent.text} ({ent.label_}), "
+        elif ent.label_ in ['DATE']:
+            english_df.at[i, 'DATE_Entity'] += f"{ent.text} ({ent.label_}), "
+        elif ent.label_ in ['TIME']:
+            english_df.at[i, 'TIME_Entity'] += f"{ent.text} ({ent.label_}), "
+        elif ent.label_ in ['GPE', 'LOC','FAC']:
+            english_df.at[i, 'LOC_Entity'] += f"{ent.text} ({ent.label_}), "
+        elif ent.label_ in ['NORP']:
+            english_df.at[i, 'NORP_Entity'] += f"{ent.text} ({ent.label_}), "
 # %%
-english_df
+english_df.columns
+# %%
+
+df2 = english_df.filter(['CampaignURL','SportName','City','State','Country','TeamOrAthlete','language','PERSON_Entity', 'ORG_Entity','PRODUCT_Entity', 'EVENT_Entity', 'MONEY_Entity', 'DATE_Entity','TIME_Entity', 'GPE_Entity', 'LOC_Entity', 'NORP_Entity'], axis=1)
+print(df2)
 # %%
 # write the results to a new CSV file
-english_df.to_csv("{}\\data\\text_analysis_data\\ner_results.csv".format(cwd))
+df2.to_csv("{}\\data\\text_analysis_data\\ner_results.csv".format(cwd))
 
 
 # %%
