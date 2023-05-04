75d29a9338a27873fc7c13b6e57b35c14bcb4d36
Ashutosh-Maharana <ashutosh.maharana@okstate.edu>
2023-05-04 18:32:02 -0500

    modified:   code/text_analysis/named_entity_recognition.py 	modified:   README.md 	modified:   data/text_analysis_data/ner_results.csv

--- code/text_analysis/named_entity_recognition.py
+++ code/text_analysis/named_entity_recognition.py
@@ -9,7 +9,7 @@ nlp = spacy.load('en_core_web_sm')
 # %%
 import os
 cwd = os.getcwd()
-cwd = cwd.replace("\\code", "")
+cwd = cwd.replace("\\code\\text_analysis", "")
 cwd
 # %%
 df = pd.read_csv("{}\\data\\clean_data\\final_dataset_textanalysis_sentiment_score.csv".format(cwd))
@@ -49,11 +49,52 @@ for i, story in english_df['Story_Original'].iteritems():
 english_df.columns
 # %%
 
-df2 = english_df.filter(['CampaignURL','SportName','City','State','Country','TeamOrAthlete','language','PERSON_Entity', 'ORG_Entity','PRODUCT_Entity', 'EVENT_Entity', 'MONEY_Entity', 'DATE_Entity','TIME_Entity', 'GPE_Entity', 'LOC_Entity', 'NORP_Entity'], axis=1)
+df2 = english_df.filter(['CampaignURL','SportName','City','State','Country','TeamOrAthlete','language','FundsRaisedPercent','PERSON_Entity', 'ORG_Entity','PRODUCT_Entity', 'EVENT_Entity', 'MONEY_Entity', 'DATE_Entity','TIME_Entity', 'GPE_Entity', 'LOC_Entity', 'NORP_Entity'], axis=1)
 print(df2)
+
+#%%
+
+# success column
+# create function to map moneyraised tobinary values
+def map_success(FundsRaisedPercent):
+    if FundsRaisedPercent >= 100:
+        return 1
+    else:
+        return 0
+
+# apply function to create new column
+df2['IsSuccess'] = df2['FundsRaisedPercent'].apply(map_success)
+
+#%%
+pd.set_option('display.max_columns', None)
+df2.head
+
 # %%
 # write the results to a new CSV file
 df2.to_csv("{}\\data\\text_analysis_data\\ner_results.csv".format(cwd))
 
+
+# %%
+
+import spacy
+from spacy import displacy
+import pandas as pd
+# %%
+# load spaCy's English model
+nlp = spacy.load('en_core_web_sm')
+# %%
+# read the CSV file containing the text data
+df = english_df
+# %%
+# choose a story to visualize the named entities
+CampaignURL = "https://sportfunder.com/hunterpowershowcase/28192"
+row_index = df.loc[df['CampaignURL'] == CampaignURL].index[0] # find the row index of the specified CampaignURL
+story_text = df.loc[row_index, 'Story_Original']
+
+# perform named entity recognition using spaCy
+doc = nlp(story_text)
+# %%#
+# visualize the named entities using displacy
+displacy.render(doc, style='ent', jupyter=True)
 
 # %%
